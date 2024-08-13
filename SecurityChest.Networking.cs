2df9a5ea198f368b2b04b91e01ea48e9fd52a95e
oni2302 <65790507+nnp2302@users.noreply.github.com>
2024-08-13 23:52:35 +0700

    init

---
+++ SecurityChest.Networking.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SecurityChest
+{
+    partial class SecurityChest
+    {
+        public override void HandlePacket(BinaryReader reader, int whoAmI)
+        {
+            if (Main.netMode == NetmodeID.Server)
+            {
+                int i = reader.ReadInt32();
+                int j = reader.ReadInt32();
+                int placingPlayerID = reader.ReadInt32();
+                ModPacket packet = GetPacket();
+                packet.Write("helllo");
+                packet.Send(ignoreClient:255);
+
+                Logger.Info($"(Client) Received placement data: Tile at ({i}, {j}) placed by player ID: {placingPlayerID}");
+            }
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                string s = reader.ReadString();
+                Main.NewText("R F S" + s);
+            }
+            base.HandlePacket(reader, whoAmI);
+        }
+    }
+}
---
+++ SecurityChest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace SecurityChest
+{
+	public partial class SecurityChest : Mod
+	{
+
+	}
+}
